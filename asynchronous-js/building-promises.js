b3ad02a70c0affd677782a295f94bc799b06705d
subrato29 <subratosarkar29@gmai.com>
2023-01-22 13:41:54 -0500

    updated code

--- asynchronous-js/building-promises.js
+++ asynchronous-js/building-promises.js
@@ -20,23 +20,17 @@ const writeFilePro = (file, data) => {
 }
 
 /**
- * `await` keyword stop the code until promise is resolved.
- *  One `await` represent one `promise` and `then()` process.
- * `async-await` make code synchronized where as the code remains asynchronous behind the scene.
+ * `return` keyword returns promises and that get caught by `then` keyword
  */
-const getResponse = async() => {
-    try {
-        const data = await readFilePro(`${__dirname}/input.txt`);
+readFilePro(`${__dirname}/input.txt`)
+    .then(data => {
         console.log(`The input is: ${data}`);
-
-        const res = await superagent.get(`https://gorest.co.in/public/v2/users/${data}`);
+        return superagent.get(`https://gorest.co.in/public/v2/users/${data}`);
+    }).then(res => {
         console.log(res.body.name);
-
-        await writeFilePro('response.txt', res.body.name); 
-        console.log('Response message saved');   
-    } catch (err) {
+        return writeFilePro('response.txt', res.body.name);
+    }).then(() => {
+        console.log('Response message saved');
+    }).catch(err => {
         console.log(`err message: ${err.message}`);
-    }
-}
-
-getResponse();
+    });
