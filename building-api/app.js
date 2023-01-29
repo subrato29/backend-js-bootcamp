84d8d17271113a2c009f61a079e962e0342b1a62
subrato29 <subratosarkar29@gmai.com>
2023-01-28 21:22:40 -0500

    updated code

--- building-api/app.js
+++ building-api/app.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!')
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours'})
 });
+
+app.post('/', (req, res) => {
+  res.send('You can post in this enpoint...');
+})
 
 const port = 3000;
 app.listen(port, () => {
