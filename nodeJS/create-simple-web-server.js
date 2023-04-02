7eb2c62855437d958e0c32df1cefd791d458c2be
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-04-02 11:51:57 -0400

    updated code

--- nodeJS/create-simple-web-server.js
+++ nodeJS/create-simple-web-server.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const fs = require('fs');
 const http = require('http');
 
-const port = 8000;
+const PORT = 8000;
+const HOST = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
+    console.log(req);
     res.end('Hello from the server!');
 });
 
-server.listen(port, '127.0.0.1', () => {
-    console.log(`Listening to request on port ${port}`);
+server.listen(PORT, HOST, () => {
+    console.log(`Listening to request on port ${PORT}`);
 })
