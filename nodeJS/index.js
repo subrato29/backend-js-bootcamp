db508835b620a4038b6d8188ab0f8a5496a7d5fe
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-04-02 10:29:05 -0400

    updated code

--- nodeJS/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs') // 'fs' stands for file system
-
-const txtIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(txtIn);
-
-const txtOut = `The output is: ${txtIn}. \nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', txtOut);
-console.log('File written!');
---
+++ nodeJS/reading-and-writing-files-asynchronously.js
@@ -0,0 +1,9 @@
+const fs = require('fs') // 'fs' stands for file system
+
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+    if(err) console.log('ERROR!')
+    fs.writeFile('./txt/output.txt', `The output is:\n${data}`, 'utf-8', err => {
+        console.log('Your file has been written')
+    })
+})
+console.log('Will read file!');
