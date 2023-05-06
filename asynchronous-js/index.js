eb88bfa22e0d88a88392ba4e5575e992ad393ad1
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-05-06 15:26:45 -0400

    promise

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,23 +1,39 @@
+const { Console } = require('console')
 const fs = require('fs')
 const superagent = require('superagent')
 
 const BASEURL = 'https://gorest.co.in'
 
-fs.readFile(`${__dirname}/input.txt`, (err, data) => {
-  console.log(`The output is ${data}`)
-
-  superagent
-    .get(`${BASEURL}/public/v2/users/${data}`)
-    .then((res) => {
-      if (err) return console.log(err)
-      console.log(res.body)
-
-      fs.writeFile('response.txt', res.body.email, (err) => {
-        if (err) return console.log(err)
-        console.log(`The extracted email from the response body is saved`)
-      })
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find the file')
+      resolve(data)
     })
-    .catch((err) => {
-      console.log(err.message)
+  })
+}
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file')
+      resolve('Success!')
     })
-})
+  })
+}
+
+readFilePro(`${__dirname}/input.txt`)
+  .then((data) => {
+    console.log(`The output is ${data}`)
+    return superagent.get(`${BASEURL}/public/v2/users/${data}`)
+  })
+  .then((res) => {
+    console.log(res.body)
+    return writeFilePro('response.txt', res.body.email)
+  })
+  .then(() => {
+    console.log('The response saved into the file!')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
